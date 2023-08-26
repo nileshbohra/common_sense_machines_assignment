ac8c3f6c7dbb34f80ae3b46291e20d3f38c1989e
Nilesh <nmbohra2000@gmail.com>
2023-08-26 09:31:09 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(db => console.log('DB is connected')).catch(err => console.error(err));
+}).then(db => console.log('Successfully connected to DB')).catch(err => console.error(err));
 
 app.use(cors());
 app.use(express.json());
@@ -17,4 +17,5 @@ app.use('/api/users', require('./routes/user.route'));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
+    
 });
